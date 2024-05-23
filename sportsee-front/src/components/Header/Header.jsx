970af965851a54bb545c95dbcb6bee56631503ae
youtlane <youtlanehanane@gmail.com>
2024-05-23 18:26:32 +0200

    feat: header nav

--- sportsee-front/src/components/Header/Header.jsx
+++ sportsee-front/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from '../Nav/Nav';
-import logo from '../../assets/big-logo.svg';
+import logo from '../../assets/big-logo.svg'
+
 
 const Header = () => {
     const headerItems = [
@@ -12,7 +13,11 @@ const Header = () => {
     
     return (
         <header className="header">
-            <Nav items={headerItems} logo={logo} isHeader={true} />
+            <div className="header-logo">
+                <img src={logo} alt="Logo" className="logo" />
+            </div>
+            
+            <Nav items={headerItems} isHeader={true} />
         </header>
     );
 };
--- sportsee-front/src/components/Nav/Nav.jsx
+++ sportsee-front/src/components/Nav/Nav.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 
-
-const Nav = ({ items, logo, isHeader }) => {
+const Nav = ({ items, isHeader }) => {
     return (
         <nav className={isHeader ? "nav-header" : "nav-aside"}>
-            {logo && isHeader && <img src={logo} alt="Logo" className="nav-logo" />}
-            <ul className={isHeader ? "nav-header_list" : "nav-aside_list"}>
-                {items.map((item, index) => (
-                    <a key={index} className="nav-item" href={item.link}>
-                        {isHeader ? item.text : <img src={item.icon} alt={item.alt} className="nav-icon" />}
-                    </a>
-                ))}
-            </ul>
+            {items.map((item, index) => (
+                <a key={index} className={isHeader ? "nav-header_item" : "nav-aside_item"} href={item.link}>
+                    {isHeader ? item.text : <img src={item.icon} alt={item.alt} className="nav-icon" />}
+                </a>
+            ))}
         </nav>
     );
 };
 
 export default Nav;
-
-
