c4fe0349c98acc98452cf5fc457fe65e2c110395
youtlane <youtlanehanane@gmail.com>
2024-05-30 16:37:54 +0200

    feat: styling average sessions chart

--- sportsee-front/src/components/AverageSessions/AverageSessions.jsx
+++ sportsee-front/src/components/AverageSessions/AverageSessions.jsx
@@ -1,46 +1,78 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-const dayAbbreviations = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
+import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer, Rectangle } from 'recharts';
 
 const AverageSessions = ({ averageSessionsData }) => {
-    // Fonction de formatage des jours de la semaine
     const dayFormatter = (tick) => {
+        const dayAbbreviations = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
         return dayAbbreviations[tick - 1];
     };
 
-    // Personnalisation du contenu du tooltip
+    // Custom tooltip content
     const CustomTooltip = ({ active, payload }) => {
         if (active && payload && payload.length) {
             const data = payload[0].payload;
             return (
                 <div className="custom-tooltip">
-                    <p className="label">{`${data.sessionLength} min`}</p>
+                    <p className="label-time">{`${data.sessionLength} min`}</p>
                 </div>
             );
         }
-
         return null;
+    };
+
+    // Custom cursor
+    const CustomizedCursor = ({ points }) => {
+        return <Rectangle fill="#000000" opacity={0.1} x={points[0].x} width={258} height={263} />;
     };
 
     return (
         <div className="average-sessions-container">
             <h2 className="average-sessions-title">Durée moyenne des sessions</h2>
-            <ResponsiveContainer width="100%" height={300}>
+            <ResponsiveContainer width="100%" height="100%">
                 <LineChart
+                    
                     data={averageSessionsData}
-                    margin={{ top: 20, right: 30, left: 20, bottom: 10 }}
+                    margin={{ top: 50, right: 0, left: 0, bottom: 10 }}
                 >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="day" tickFormatter={dayFormatter} domain={[1, 7]} />
-                    <YAxis hide />
-                    <Tooltip content={<CustomTooltip />} />
-                    <Legend />
-                    <Line type="monotone" dataKey="sessionLength" stroke="white" activeDot={{ r: 8 }} />
+                    <defs>
+                        <linearGradient
+                            id="paint0_linear_2_122"
+                            x1="309.906"
+                            y1="-1.97779"
+                            x2="-47.7754"
+                            y2="-1.97779"
+                            gradientUnits="userSpaceOnUse"
+                        >
+                            <stop stopColor="white" />
+                            <stop offset="0.810441" stopColor="white" stopOpacity="0.403191" />
+                        </linearGradient>
+                    </defs>
+                    <XAxis
+                        dataKey="day"
+                        stroke="#ffffff81"
+                        padding={{ left: 5 , right: 5}}
+                        axisLine={false}
+                        tickLine={false}
+                        tick={{ fill: 'white' }}
+                        tickFormatter={dayFormatter}
+                        interval={0}//Cela garantit que chaque point de données de 
+                                    //l'axe X est rendu, ce qui peut aider à forcer 
+                                    //l'affichage du premier jour correctement.
+                    />
+                    <Tooltip content={<CustomTooltip />} cursor={<CustomizedCursor />} />
+                    <Line
+                        type="natural"
+                        dataKey="sessionLength"
+                        stroke="url(#paint0_linear_2_122)"
+                        strokeWidth={2}
+                        activeDot={{ stroke: '#ffffff33', strokeWidth: 12, r: 5 }}
+                        dot={false}
+                        animationEasing="ease-in-out"
+                    />
                 </LineChart>
             </ResponsiveContainer>
         </div>
     );
-}
+};
 
 export default AverageSessions;
