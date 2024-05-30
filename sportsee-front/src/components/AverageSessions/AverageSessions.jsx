65ce4f124b0e13c74362fa03b236108a927c9f28
youtlane <youtlanehanane@gmail.com>
2024-05-30 15:09:32 +0200

    feat: add time of the sessions on survol on the chart

--- sportsee-front/src/components/AverageSessions/AverageSessions.jsx
+++ sportsee-front/src/components/AverageSessions/AverageSessions.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
+const dayAbbreviations = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
+
 const AverageSessions = ({ averageSessionsData }) => {
+    // Fonction de formatage des jours de la semaine
+    const dayFormatter = (tick) => {
+        return dayAbbreviations[tick - 1];
+    };
+
+    // Personnalisation du contenu du tooltip
+    const CustomTooltip = ({ active, payload }) => {
+        if (active && payload && payload.length) {
+            const data = payload[0].payload;
+            return (
+                <div className="custom-tooltip">
+                    <p className="label">{`${data.sessionLength} min`}</p>
+                </div>
+            );
+        }
+
+        return null;
+    };
+
     return (
         <div className="average-sessions-container">
             <h2 className="average-sessions-title">Durée moyenne des sessions</h2>
@@ -11,11 +32,11 @@ const AverageSessions = ({ averageSessionsData }) => {
                     margin={{ top: 20, right: 30, left: 20, bottom: 10 }}
                 >
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="day" />
-                    <YAxis />
-                    <Tooltip />
+                    <XAxis dataKey="day" tickFormatter={dayFormatter} domain={[1, 7]} />
+                    <YAxis hide />
+                    <Tooltip content={<CustomTooltip />} />
                     <Legend />
-                    <Line type="monotone" dataKey="sessionLength" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="sessionLength" stroke="white" activeDot={{ r: 8 }} />
                 </LineChart>
             </ResponsiveContainer>
         </div>
@@ -23,4 +44,3 @@ const AverageSessions = ({ averageSessionsData }) => {
 }
 
 export default AverageSessions;
-
