ff940cc2482aa1c8d4f81599b759f07f0f4cc346
youtlane <youtlanehanane@gmail.com>
2024-06-01 16:17:59 +0200

    feat: Styling radar chart

--- sportsee-front/src/components/RadarPerformance/RadarPerformance.jsx
+++ sportsee-front/src/components/RadarPerformance/RadarPerformance.jsx
@@ -4,7 +4,7 @@ import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis } from 'r
 const RadarPerformance = ({ data }) => {
     // Vérifier si les données sont disponibles
     if (!data || data.length === 0) {
-        return <div>Aucune donnée de performance disponible.</div>;
+        return <div className="radar-container">Aucune donnée de performance disponible.</div>;
     }
 
     // Formater les données pour le graphique radar
@@ -15,20 +15,20 @@ const RadarPerformance = ({ data }) => {
 
     // Créer un objet pour traduire les valeurs de kind en français
     const kindLabels = {
-        1: 'Cardio',
-        2: 'Énergie',
-        3: 'Endurance',
-        4: 'Force',
-        5: 'Vitesse',
-        6: 'Intensité',
+        1: 'Intensité',
+        2: 'Vitesse',
+        3: 'force',
+        4: 'Endurance',
+        5: 'Energie',
+        6: 'Cardio',
     };
 
     return (
-        <div className="radar-container">
-            <RadarChart cx={300} cy={250} outerRadius={150} width={600} height={500} data={radarData}>
+        <div className="radar-container" style={{ width: '258px', height: '263px', borderRadius: '5px', background: '#282D30', boxShadow: '0px 2px 4px 0px rgba(0, 0, 0, 0.02)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <RadarChart cx="50%" cy="50%" outerRadius="75%" width={258} height={263} data={radarData}>
                 <PolarGrid />
                 <PolarAngleAxis dataKey="kind" tick={{ fill: 'white', fontSize: 12 }} tickFormatter={(value) => kindLabels[value]} />
-                <PolarRadiusAxis />
+                <PolarRadiusAxis tick={false} axisLine={false} />
                 <Radar name="Performance" dataKey="value" stroke="#ff0000" fill="#ff0000" fillOpacity={0.6} />
             </RadarChart>
         </div>
